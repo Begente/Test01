252a4626d6daa4c03c9f3fc1d660bafd14c6c115
GrEugene <eugene7007@yandex.com>
2018-04-19 18:28:11 +0500

    change main method 2

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,6 +1,6 @@
 public class Start {
 	public static void main(String[] args) {
-		String text = "Fork Git. Hello World!!!";
+		String text = "Third commit. Fork Git. Hello World!!!";
 		System.out.println(text);
 	}
 
